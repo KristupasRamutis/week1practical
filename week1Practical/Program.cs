059436f9035c595369adc8668ee115e96d1dcf6e
Kristupas Ramutis <Ramutis-K@ulster.ac.uk>
2024-09-24 15:59:59 +0100

    task 3

--- week1Practical/Program.cs
+++ week1Practical/Program.cs
@@ -1,32 +1,27 @@
 ﻿
-    string name;
-    int age;
+    
+    int num;
 
 
 
-Console.WriteLine("please enter your name");
-    name = Console.ReadLine();
-    Console.WriteLine("please enter your age");
-    age = Convert.ToInt32(Console.ReadLine());
 
-if (age > 19)
+    Console.WriteLine("please enter your number");
+    num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
 {
-    int result = age + 5;
-    Console.WriteLine("hello {0} you are going to be {1} in 5 years and you are a Adult", name, result);
+  
+    Console.WriteLine("hello {0} is negative",num);
 }
-else if (age <= 19 && age >= 13)
+else if (num > 0)
 {
-    int result = age + 5;
-    Console.WriteLine("hello {0} you are going to be {1} in 5 years and you are a teenager", name, result);
+   
+    Console.WriteLine("hello {0} is positive", num);
 }
-else if (age < 13)
+else if (num == 0)
 {
-    int result = age + 5;
-    Console.WriteLine("hello {0} you are going to be {1} in 5 years and you are a child", name, result);
-}
-else 
-{
-    Console.WriteLine("you did not enter a valid age");
+    
+    Console.WriteLine("your number is {0}", num);
 }
 
 
